35c317deccc9c8fb817ca7193054084988516878
Ardel Tioco Jeff Lauron <ardeltiocojefflauron@gmail.com>
2024-04-22 22:07:28 +0800

    Added show password on login

--- src/main/java/com/example/csit228f2_2/LoginPage.java
+++ src/main/java/com/example/csit228f2_2/LoginPage.java
@@ -5,12 +5,15 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,8 +33,33 @@ public class LoginPage implements Initializable {
     PasswordField password_login;
     @FXML
     Label error_message;
+    @FXML
+    Button show_password_btn;
+    @FXML
+    HBox password_container;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        TextField textField = new TextField();
+        textField.setVisible(false);
+
+        show_password_btn.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                textField.setVisible(true);
+                textField.setText(password_login.getText());
+                password_container.getChildren().remove(password_login);
+                textField.setPrefWidth(291);
+                password_container.getChildren().add(textField);
+            }
+        });
+        show_password_btn.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                password_container.getChildren().add(password_login);
+                password_container.getChildren().remove(textField);
+            }
+        });
+
         forget_password.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
